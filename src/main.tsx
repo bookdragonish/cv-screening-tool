7c7c6f696fb5bd409a9a65c0eac56f157950296e
Mohammad Kazem Khajeh <kazem_kh95@hotmail.com>
2026-01-22 20:01:06 +0100

    refactor: set up react router and structure folders in project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './css/main.css'
+import Home from './pages/Home.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
