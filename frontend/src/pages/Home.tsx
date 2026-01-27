f44f0f74b6459c92e3cbcec3b2979008992b6e46
Marius Fornes <mariufor@stud.ntnu.no>
2026-01-27 14:22:05 +0100

    [feat] Added a PDF only upload button (#4)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <label className="block mb-2 text-sm font-medium text-black">
+        <label className="block mb-2 text-sm font-medium text-blue-700">
           Upload resumes here:
         </label>
         <input type="file" accept="application/pdf" className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-300 focus:outline-none"/>
