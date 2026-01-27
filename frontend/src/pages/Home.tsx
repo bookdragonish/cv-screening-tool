4fe01b67607a5a75adc2a8633237a45bc66bb26d
Marius Fornes <mariufor@stud.ntnu.no>
2026-01-27 14:12:43 +0100

    [feat] Added a button for PDF upload (#4)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <div>
+        <label className="block mb-2 text-sm font-medium text-black">
+          Upload resumes here:
+        </label>
+        <input type="file" accept="application/pdf" className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-300 focus:outline-none"/>
+        
 
       </div>
     </>
