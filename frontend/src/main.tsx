5e3ccc3dc82a2fd6c9cd6a91c426f78b171b9240
Mohammad Kazem Khajeh <kazem_kh95@hotmail.com>
2026-01-27 12:37:25 +0100

    feat: create layout component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './lib/styles/globals.css'
-import Home from './pages/Home.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import '@/lib/styles/globals.css'
+import Home from '@/pages/Home'
+import Layout from './pages/Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
         </Routes>
     </BrowserRouter>
   </StrictMode>,
