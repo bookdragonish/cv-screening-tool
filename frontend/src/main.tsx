b69866eb6e78dc56ef145d672698dbf806b56524
Mohammad Kazem Khajeh <kazem_kh95@hotmail.com>
2026-02-28 18:49:24 +0100

    feat: add routes for pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,10 @@ import '@/lib/styles/globals.css'
 import Home from '@/pages/Home'
 import Layout from '@/pages/Layout'
 import GeminiTestPage from '@/pages/GeminiTestPage'
-import ScreeningFlowPage from '@/pages/screening/ScreeningFlowPage'
-import NewScreeningPage from '@/pages/screening/NewScreeningPage'
-import ScreeningResultsPage from '@/pages/screening/ScreeningResultsPage'
-import ScreeningCandidatePage from '@/pages/screening/ScreeningCandidatePage'
+import ScreeningFlowPage from '@/pages/newScreening/ScreeningFlowPage'
+import NewScreeningPage from '@/pages/newScreening/NewScreeningPage'
+import ScreeningResultsPage from '@/pages/newScreening/ScreeningResultsPage'
+import ScreeningCandidatePage from '@/pages/newScreening/ScreeningCandidatePage'
 import Screening from '@/pages/Screening'
 import ScreeningHistory from '@/pages/ScreeningHistory'
 import CVDatabase from '@/pages/CVDatabase'
@@ -20,13 +20,15 @@ createRoot(document.getElementById('root')!).render(
           <Route element={<Layout />}>
             <Route index element={<Home />} />
             <Route  path="GeminiTestPage" element={<GeminiTestPage />}/>
-            <Route path="/screening-flow" element={<ScreeningFlowPage />}>
+
+            <Route path="/screening" element={<ScreeningFlowPage />}>
               <Route index element={<Navigate to="new-screening" replace />} />
               <Route path="new-screening" element={<NewScreeningPage />} />
               <Route path="results" element={<ScreeningResultsPage />} />
               <Route path="results/:candidateId" element={<ScreeningCandidatePage />} />
             </Route>
-            <Route path="/screening" element={<Screening />} />
+
+            {/*<Route path="/screening" element={<Screening />} /> */}
             <Route path="/screening-historikk" element={<ScreeningHistory />} />
             <Route path="/screening-historikk/:jobPostId" element={<Screening />} />
             <Route path="/cv-database" element={<CVDatabase />} />
