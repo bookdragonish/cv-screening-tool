3c3b90d9c080e44f912c82f99c990372bff17752
Marius Fornes <mariufor@stud.ntnu.no>
2026-04-16 20:53:25 +0200

    [fix] Translated comments from norwegian to english (#100)

--- e2e/screening-flow.spec.ts
+++ e2e/screening-flow.spec.ts
@@ -1,33 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('tests screening flow', async ({ page }) => {
     
     test.setTimeout(300000);
-    // Gå til nettsiden
+    // Navigate to the website
     await page.goto('http://localhost:5173/login');
 
     await page.getByRole('button', { name: 'Logg inn' }).click();
 
-    // Skjekker om vi faktisk kom til hovedsiden. Forventer å se teksten "Velkommen tilbake".
+    // Check if we successfully navigated to the home page. Expects to see the text "Velkommen tilbake".
     await expect(page.getByRole('heading', { name: 'Velkommen tilbake!' })).toBeVisible();
 
-    // Navigerer til opprettelse av ny screening siden
+    // Navigates to the page for creating of a new screening
     await page.getByRole('link', { name: 'Ny skanning' }).click();
 
-    // Last opp stillingsannonse i pdf format
+    // Uploads a job advertisment in .pdf format
     await page.setInputFiles('input[type="file"]', 'e2e/test-data/job-description.pdf');
     await expect(page.getByRole('button', { name: 'Neste' })).toBeEnabled();
 
-    // Starter skanningen
+    // Starts scanning
     await page.getByRole('button', { name: 'Neste' }).click();
     const seResultater = page.getByRole('link', { name: 'Se resultater' });
     await expect(seResultater).toBeVisible({ timeout: 180000 });
     
-    // Navigerer til resultatsiden
+    // Navigates to result page
     await seResultater.click();
     
 
-    // Skjekker at vi ender på rikitg side (resultatsiden). Forventer å se f.eks teksten "Kvalifiserte kandidater".
+    // Checks if we end up on the correct page (result page). Expects to see for example the text "Kvalifiserte kandidater" and "Ikke kvalifiserte kandidater".
     await expect(page.getByRole('heading', { name: 'Kvalifiserte kandidater', exact: true, level: 2 })).toBeVisible({ timeout: 15000 });
     await expect(page.getByRole('heading', { name: 'Ikke kvalifiserte kandidater', exact: true, level: 2 })).toBeVisible({ timeout: 15000 });
 
